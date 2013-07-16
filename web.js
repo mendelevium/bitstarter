b7cdfdfd0f84045932adfcad7eeaf4f320bf5293
Martin Dionne <martindionne@gmail.com>
2013-07-16 18:30:00 -0400

    hw4 part 1

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World!');
-  response.send(fs.readFileSync('index.html').toString('utf-8'));
+  response.send(fs.readFileSync(htmlfile).toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+// var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
